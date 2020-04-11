6a84ab0f053f06b71e4da6822f7ab4dcf482ad88
IliasMariosG <ilias_grigorop2@htomail.com>
2020-04-11 23:57:22 +0100

    fix  method name

--- spec/note-formatter_spec.rb
+++ spec/note-formatter_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'note-formatter'
 
 describe NoteFormatter do
-  describe '#display' do
+  describe '#format' do
     it 'formats the title' do
       note_formatter = NoteFormatter.new
       expect(note_formatter.format).to eq 'Title: #{note.title}\n#{note.body}'
