89c6405db09eadf9bb81c85afed36d9923ee744b
IliasMariosG <ilias_grigorop2@htomail.com>
2020-04-12 12:56:30 +0100

    Add test for NoteFormatter
    Hardcode first test for format method

--- spec/note-formatter_spec.rb
+++ spec/note-formatter_spec.rb
@@ -5,7 +5,8 @@ describe NoteFormatter do
   describe '#format' do
     it 'formats the title' do
       note_formatter = NoteFormatter.new
-      expect(note_formatter.format).to eq 'Title: #{note.title}\n#{note.body}'
+      expect(note_formatter.format).to eq "Title: title
+body"
     end
   end
 end
