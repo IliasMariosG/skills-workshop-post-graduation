8f0b8e6aa7238dc1a63c85f0f55ea26a5456a697
IliasMariosG <ilias_grigorop2@htomail.com>
2020-04-16 13:20:40 +0100

    Refactor test file in expectations
    It replaces hard-coded note with note variable

--- spec/note-formatter_spec.rb
+++ spec/note-formatter_spec.rb
@@ -5,8 +5,8 @@ describe NoteFormatter do
   describe '#format' do
     it 'formats the title' do
       note_formatter = NoteFormatter.new
-      expect(note_formatter.format).to eq "Title: title
-body"
+      note = "Title: title\nbody"
+      expect(note_formatter.format(note)).to eq "Title: title\nbody"
     end
   end
 end
